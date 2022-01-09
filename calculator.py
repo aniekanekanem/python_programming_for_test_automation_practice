4c8464a4a8371204e07cf17259796823814d7241
charlesekanemm <ekanemca@yahoo.com>
2022-01-09 22:08:35 +0100

    Add multiplication

--- calculator.py
+++ calculator.py
@@ -10,5 +10,11 @@ def subtraction():
     b = float(input("Enter another Number"))
     print(a - b)    
 
+def multiplication():
+    a = float(input("Enter a Number"))
+    b = float(input("Enter another Number"))
+    print(a * b)
+
 addition()
-subtraction()    
+subtraction()
+multiplication()    
